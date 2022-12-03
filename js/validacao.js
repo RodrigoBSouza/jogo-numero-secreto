ecdded24a6bdd9d92ad0452fc80fdbdb6b5b8fee
RodrigoBSouza <rodrigbsouza@gmail.com>
2022-12-03 11:34:19 -0300

    jogo-numero-secreto

--- js/validacao.js
+++ js/validacao.js
@@ -15,15 +15,15 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
-            <button id="jogarNovamente" class="btn-jogar">Jogar novamente</button>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `;
     } else if(numero > numeroSecreto) {
         elementoChute.innerHTML += `
-        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div> -->
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `;
     } else {
         elementoChute.innerHTML += `
-        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div> -->
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `;
     }
 }
